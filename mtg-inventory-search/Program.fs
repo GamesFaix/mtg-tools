2d1e64c6c4c9f1b5586be3fd297dd13562971cd6
James Faix <jamesthefaix@gmail.com>
2021-08-07 01:42:48 -0700

    Wrote code to query scryfall

--- mtg-inventory-search/Program.fs
+++ mtg-inventory-search/Program.fs
@@ -1,8 +1,25 @@
-﻿// Learn more about F# at http://fsharp.org
-
-open System
+﻿open System
+open ScryfallApi.Client.Models
+open System.Net.Http
 
 [<EntryPoint>]
-let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+let main _ =
+    async {
+        use client = new HttpClient()
+        client.BaseAddress <- new Uri("https://api.scryfall.com/")
+        let scryfall = new ScryfallApi.Client.ScryfallApiClient(client)
+
+        let options = SearchOptions()
+
+        let! result = scryfall.Cards.Search("o:flip", 1, options) |> Async.AwaitTask
+        
+
+        printfn "Found %i cards" result.TotalCards
+
+        for c in result.Data do
+            printfn "\t%s" c.Name
+        
+        Console.Read() |> ignore
+        return 0 // return an integer exit code
+    } |> Async.RunSynchronously
+    
