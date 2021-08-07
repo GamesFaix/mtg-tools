ad3861fe74b8941c868b7ec047c891ca2f6e0647
James Faix <jamesthefaix@gmail.com>
2021-08-07 02:05:22 -0700

    Added CSV reader

--- mtg-inventory-search/Program.fs
+++ mtg-inventory-search/Program.fs
@@ -1,9 +1,13 @@
 ﻿open System
 open ScryfallApi.Client.Models
 open System.Net.Http
+open System.IO
+open CsvHelper
+open CsvHelper.Configuration
+open System.Globalization
+open System.Linq
 
-[<EntryPoint>]
-let main _ =
+let searchScryfall query = 
     async {
         use client = new HttpClient()
         client.BaseAddress <- new Uri("https://api.scryfall.com/")
@@ -11,13 +15,50 @@ let main _ =
 
         let options = SearchOptions()
 
-        let! result = scryfall.Cards.Search("o:flip", 1, options) |> Async.AwaitTask
-        
+        return! scryfall.Cards.Search(query, 1, options) |> Async.AwaitTask
+    }
 
-        printfn "Found %i cards" result.TotalCards
+type InventoryCard = {
+    count: int
+    name: string
+    set: string    
+}
 
-        for c in result.Data do
-            printfn "\t%s" c.Name
+let loadInventory () : InventoryCard list =
+    let path = sprintf "%s/inventory1 - main.csv" (Environment.GetFolderPath(Environment.SpecialFolder.Desktop))
+    use reader = File.OpenText path :> TextReader
+    let config = CsvConfiguration(CultureInfo.InvariantCulture)
+    config.HasHeaderRecord <- false
+    use csv = new CsvReader(reader, config)
+    csv.Read() |> ignore
+    let cards = csv.GetRecords<InventoryCard>()
+    cards |> Seq.toList
+
+[<EntryPoint>]
+let main _ =
+    async {
+        let! result = searchScryfall "o:proliferate"
+        let inventory = loadInventory ()
+        let joined = 
+            Enumerable.Join(
+                result.Data,
+                inventory,
+                (fun x -> (x.Name.ToLowerInvariant(), x.Set.ToLowerInvariant())),
+                (fun y -> (y.name.ToLowerInvariant(), y.set.ToLowerInvariant())),
+                (fun x y -> (x, y))
+            )
+
+        //printfn "Found %i cards" result.TotalCards
+
+        //for c in result.Data do
+        //    printfn "\t%s" c.Name
+
+        //for c in inventory |> Seq.take 10 do
+        //    printfn "\t%s %s" c.set c.name
+
+            
+        for (x,y) in joined |> Seq.take 10 do
+            printfn "\t%s %s" x.Set x.Name
         
         Console.Read() |> ignore
         return 0 // return an integer exit code
