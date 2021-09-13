ceb203776153c6bb434753383174163057ae7239
James Faix <jamesthefaix@gmail.com>
2021-09-13 01:39:22 -0700

    Import ShandalarToCockatrice

---
+++ ShandalarToCockatrice/Model.cs
@@ -0,0 +1,31 @@
+﻿namespace ShandalarToCockatrice
+{
+    public class DeckItem
+    {
+        public string Name;
+        public int Count;
+
+        public override string ToString() =>
+            $"{Count} {Name}";
+    }
+
+    public class Deck
+    {
+        public string Name;
+        public string Comments;
+        public DeckItem[] Cards;
+        public DeckItem[] Sideboard;
+    }
+
+    public class ShandalarDeckModel
+    {
+        public string Name;
+        public DeckItem[] Core;
+        public DeckItem[] DefaultExtension;
+        public DeckItem[] BlackExtension;
+        public DeckItem[] BlueExtension;
+        public DeckItem[] GreenExtension;
+        public DeckItem[] RedExtension;
+        public DeckItem[] WhiteExtension;
+    }
+}
