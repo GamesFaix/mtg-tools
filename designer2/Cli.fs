599c2622c9abf9b5bd6d0559bb35ef77cf90e401
James Faix <jamesthefaix@gmail.com>
2021-09-16 08:44:22 -0700

    Added cli

---
+++ designer2/Cli.fs
@@ -0,0 +1,65 @@
+﻿module GamesFaix.MtgTools.Designer.Cli
+
+open Argu
+open Model
+
+type CardArguments =
+    | [<CliPrefix(CliPrefix.None)>] Clone of oldSet:string * oldCard:string * newSet:string * newCard:string
+    | [<CliPrefix(CliPrefix.None)>] Delete of set:string * card:string
+    | [<CliPrefix(CliPrefix.None)>] DownloadImage of set:string * card:string
+
+    interface IArgParserTemplate with
+        member this.Usage =
+            match this with
+            | Clone _ -> "Copies a card"
+            | Delete _ -> "Deletes a card"
+            | DownloadImage _ -> "Downloads the image of a card"
+
+type SetArguments =
+    | [<CliPrefix(CliPrefix.None)>] Audit of set:string
+    | [<CliPrefix(CliPrefix.None)>] AutoNumber of set:string
+    | [<CliPrefix(CliPrefix.None)>] Clone of oldSet:string * newSet:string
+    | [<CliPrefix(CliPrefix.None)>] CreateHtmlLayout of set:string
+    | [<CliPrefix(CliPrefix.None)>] Delete of set:string
+    | [<CliPrefix(CliPrefix.None)>] DownloadImages of set:string
+    | [<CliPrefix(CliPrefix.None)>] Rename of oldSet:string * newSet:string
+
+    interface IArgParserTemplate with
+        member this.Usage =
+            match this with
+            | Audit _ -> "Audits a set for anomalies"
+            | AutoNumber _ -> "Updates the collector's number on all cards in a set"
+            | Clone _ -> "Copies a set"
+            | CreateHtmlLayout _ -> "Creates an HTML layout file for printing a set"
+            | Delete _ -> "Deletes a set"
+            | DownloadImages _ -> "Downloads images for each card in a set"
+            | Rename _ -> "Renames a set"
+
+type MainArguments =
+    | [<CliPrefix(CliPrefix.None)>] Card of CardArguments ParseResults
+    | [<CliPrefix(CliPrefix.None)>] Set of SetArguments ParseResults
+
+    interface IArgParserTemplate with
+        member this.Usage =
+            match this with
+            | Card _ -> "Allows manipulating individual cards"
+            | Set _ -> "Allows manipulating sets (expansions) of cards"
+
+let getJob (ctx: Context) (results: MainArguments ParseResults) : unit Async =
+    match results.GetAllResults().Head with
+
+    | Card results ->
+        match results.GetAllResults().Head with
+        | CardArguments.Clone (oldSet, oldCard, newSet, newCard) -> failwith "Not implemented"
+        | CardArguments.Delete (set, card) -> failwith "Not implemented"
+        | DownloadImage (set, card) -> failwith "Not implemented"
+
+    | Set results ->
+        match results.GetAllResults().Head with
+        | Audit set -> failwith "Not implemented"
+        | AutoNumber set -> failwith "Not implemented"
+        | SetArguments.Clone (oldSet, newSet) -> Macro.cloneSet ctx oldSet newSet
+        | CreateHtmlLayout set -> failwith "Not implemented"
+        | SetArguments.Delete set -> failwith "Not implemented"
+        | DownloadImages set -> failwith "Not implemented"
+        | Rename (oldSet, newSet) -> failwith "Not implemented"
--- designer2/Program.fs
+++ designer2/Program.fs
@@ -1,7 +1,12 @@
 ﻿module GamesFaix.MtgTools.Designer.Program
 
 open System
+open System.Net.Http
+open Argu
 open Microsoft.Extensions.Configuration
+open Serilog
+open Cli
+open Model
 
 type Settings = {
     OutputDirectory : string
@@ -26,5 +31,24 @@ let configure args =
 let main args =
     let settings = configure args
 
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+    let logger =
+        LoggerConfiguration()
+            .WriteTo.Console()
+            .CreateLogger()
+
+    let ctx : Context = {
+        Logger = logger
+        Http = new HttpClient()
+        RootDir = settings.OutputDirectory
+        Cookie = ""
+    }
+
+    try
+        let parser = ArgumentParser.Create<MainArguments>(programName = "designer")
+        let results = parser.Parse(inputs = args, raiseOnUsage = true)
+        let job = Cli.getJob ctx results
+        job |> Async.RunSynchronously
+        0 // return an integer exit code
+    with e ->
+        logger.Error(e, "An unexpected error occurred.")
+        1
